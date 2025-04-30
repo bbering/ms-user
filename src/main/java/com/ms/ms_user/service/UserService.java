f7002973f9e2b19f1ce146bb89416bb0425faa9d
bbering <bbering88@gmail.com>
2025-04-30 19:44:32 -0300

    feat(UserService): adicionados metodos simples para auxiliar mapear DTOs e Entidades

--- src/main/java/com/ms/ms_user/service/UserService.java
+++ src/main/java/com/ms/ms_user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.ms.ms_user.service;
 
+import org.springframework.beans.BeanUtils;
+
+import com.ms.ms_user.dtos.UserRequestDTO;
+import com.ms.ms_user.dtos.UserResponseDTO;
+import com.ms.ms_user.models.User;
 import com.ms.ms_user.repositories.UserRepository;
 
 public class UserService {
@@ -8,5 +13,17 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User toEntity(UserRequestDTO userRequest) {
+        User userMapped = new User();
+        BeanUtils.copyProperties(userRequest, userMapped);
+        return userMapped;
+    }
+
+    public UserResponseDTO toDTO(User userData) {
+        UserResponseDTO userDataToReturn = new UserResponseDTO();
+        BeanUtils.copyProperties(userData, userDataToReturn);
+        return userDataToReturn;
     }
 }
