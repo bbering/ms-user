62b3713e1f4da631a3cbd8837285da155952961f
bbering <bbering88@gmail.com>
2025-05-05 00:07:58 -0300

    feat(UserProducer): configuração da routingKey para envio de mensagens

--- src/main/java/com/ms/ms_user/producers/UserProducer.java
+++ src/main/java/com/ms/ms_user/producers/UserProducer.java
@@ -3,10 +3,15 @@ package com.ms.ms_user.producers;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Component
 public class UserProducer {
 
     final RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
 
     public UserProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
