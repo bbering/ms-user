2f220389ce92751d283d45447de3d3d6fc6de1c3
bbering <bbering88@gmail.com>
2025-05-05 00:22:06 -0300

    refactor(UserRequestDTO): adicionada uuid userID ao DTO para criar user

--- src/main/java/com/ms/ms_user/dtos/UserRequestDTO.java
+++ src/main/java/com/ms/ms_user/dtos/UserRequestDTO.java
@@ -1,11 +1,14 @@
 package com.ms.ms_user.dtos;
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRequestDTO {
+    private UUID userId;
     private String email;
     private String name;
 }
