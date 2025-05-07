25459b26f19910a98ba031d8a553d3ddc9670278
bbering <bbering88@gmail.com>
2025-05-06 22:50:30 -0300

    feat(UserProducer): criação do metodo para o producer enviar um objeto emailrequestdto para a queue do rabbitmq

--- src/main/java/com/ms/ms_user/producers/UserProducer.java
+++ src/main/java/com/ms/ms_user/producers/UserProducer.java
@@ -3,6 +3,9 @@ package com.ms.ms_user.producers;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+import com.ms.ms_user.dtos.EmailRequestDTO;
+import com.ms.ms_user.dtos.UserRequestDTO;
+
 import org.springframework.beans.factory.annotation.Value;
 
 @Component
@@ -15,6 +18,16 @@ public class UserProducer {
 
     public UserProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publishEmailMessage(UserRequestDTO userDataDTO) {
+        EmailRequestDTO emailToSendToQueue = new EmailRequestDTO();
+        emailToSendToQueue.setUserId(userDataDTO.getUserId());
+        emailToSendToQueue.setEmailTo(userDataDTO.getEmail());
+        emailToSendToQueue.setSubject("O seu cadastro foi realizado com sucesso!");
+        emailToSendToQueue.setContent("Obrigado por se cadastrar na nossa plataforma!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailToSendToQueue);
     }
 
 }
