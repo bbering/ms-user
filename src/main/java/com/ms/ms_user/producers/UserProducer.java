d905b4d39e5d3f91c38f0b8cc5a2435c6520bb39
bbering <bbering88@gmail.com>
2025-05-12 23:36:11 -0300

    feat(UserProducer): alteração na UserProducer para utilizar estritamente o DTO responsavel por eventos rabbit

--- src/main/java/com/ms/ms_user/producers/UserProducer.java
+++ src/main/java/com/ms/ms_user/producers/UserProducer.java
@@ -3,7 +3,7 @@ package com.ms.ms_user.producers;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-import com.ms.ms_user.dtos.EmailRequestDTO;
+import com.ms.ms_user.dtos.UserCreatedEventDTO;
 import com.ms.ms_user.dtos.UserRequestDTO;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -14,20 +14,19 @@ public class UserProducer {
     final RabbitTemplate rabbitTemplate;
 
     @Value(value = "${broker.queue.email.name}")
-    private String routingKey;
+    private String queueKey;
 
     public UserProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     public void publishEmailMessage(UserRequestDTO userDataDTO) {
-        EmailRequestDTO emailToSendToQueue = new EmailRequestDTO();
-        emailToSendToQueue.setUserId(userDataDTO.getUserId());
-        emailToSendToQueue.setEmailTo(userDataDTO.getEmail());
-        emailToSendToQueue.setSubject("O seu cadastro foi realizado com sucesso!");
-        emailToSendToQueue.setContent("Obrigado por se cadastrar na nossa plataforma!");
+        UserCreatedEventDTO event = new UserCreatedEventDTO();
+        event.setId(userDataDTO.getUserId());
+        event.setEmail(userDataDTO.getEmail());
+        event.setName(userDataDTO.getName());
 
-        rabbitTemplate.convertAndSend("", routingKey, emailToSendToQueue);
+        rabbitTemplate.convertAndSend(queueKey, event);
     }
 
 }
