e806daf41d058e938ee566636a4d424772d6fed8
bbering <bbering88@gmail.com>
2025-05-15 22:55:45 -0300

    feat(JWTUtil): criação da classe JWTUtil com metodo para gerar o token jwt

---
+++ src/main/java/com/ms/ms_user/security/JWTUtil.java
@@ -0,0 +1,40 @@
+package com.ms.ms_user.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class JWTUtil {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private int expirationTime;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // gerar o token
+    public String generateToken(String username){
+        return Jwts.builder()
+        .setSubject(username)
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+        .signWith(key, SignatureAlgorithm.HS256)
+        .compact();
+    }
+}
