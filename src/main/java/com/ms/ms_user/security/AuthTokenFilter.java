2f5a51c5a194eb2241392cc2a462d602ff1bb3f6
bbering <bbering88@gmail.com>
2025-05-20 21:23:23 -0300

    feat(AuthTokenFilter): implementado  metodo doFilterInternal

--- src/main/java/com/ms/ms_user/security/AuthTokenFilter.java
+++ src/main/java/com/ms/ms_user/security/AuthTokenFilter.java
@@ -2,6 +2,10 @@ package com.ms.ms_user.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -27,7 +31,21 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        try {
+            String jwtToken = parseJWT(request);
+            if (jwtToken != null && jwtUtil.validateToken(jwtToken)) {
+                String username = jwtUtil.getUsernameFromToken(jwtToken);
+                UserDetails userDetails = userService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null,
+                        userDetails.getAuthorities());
+
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            System.out.println("Cannot authenticate");
+        }
+        filterChain.doFilter(request, response);
     }
 
     public String parseJWT(HttpServletRequest request) {
