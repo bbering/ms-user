98977ae1ff105d2154435f7e0164a2f51b787faf
bbering <bbering88@gmail.com>
2025-05-22 21:00:06 -0300

    feat(WebSecurityConfig): criação das beans authmanager e passencoder auxiliares

--- src/main/java/com/ms/ms_user/security/WebSecurityConfig.java
+++ src/main/java/com/ms/ms_user/security/WebSecurityConfig.java
@@ -1,6 +1,11 @@
 package com.ms.ms_user.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.ms.ms_user.service.UserService;
 
@@ -14,6 +19,16 @@ public class WebSecurityConfig {
     public WebSecurityConfig(UserService userService, AuthEntryPoint authEntryPoint) {
         this.userService = userService;
         this.authEntryPoint = authEntryPoint;
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
