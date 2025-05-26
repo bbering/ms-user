2e65b2970084b8892a81665653cbf2eee9d77f98
bbering <bbering88@gmail.com>
2025-05-25 21:44:30 -0300

    feat(AuthController): criação da controller para rotas de autenticação

---
+++ src/main/java/com/ms/ms_user/controllers/AuthController.java
@@ -0,0 +1,10 @@
+package com.ms.ms_user.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+}
