068be785de6c9acb10e8600f330418f49968c117
bbering <bbering88@gmail.com>
2025-05-25 21:50:17 -0300

    feat(AuthController): criação da rota para cadastrar user

--- src/main/java/com/ms/ms_user/controllers/AuthController.java
+++ src/main/java/com/ms/ms_user/controllers/AuthController.java
@@ -1,12 +1,19 @@
 package com.ms.ms_user.controllers;
 
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ms.ms_user.dtos.UserRequestDTO;
+import com.ms.ms_user.dtos.UserResponseDTO;
+import org.springframework.security.authentication.*;
 import com.ms.ms_user.repositories.UserRepository;
 import com.ms.ms_user.security.JWTUtil;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,6 +33,11 @@ public class AuthController {
         this.userRepository = userRepository;
         this.passEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<UserResponseDTO> authUser(@Valid @RequestBody UserRequestDTO user){
+        //
     }
 
 }
