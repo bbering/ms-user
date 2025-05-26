69414951325cc8815a5fbb3174bc40885f35eb1d
bbering <bbering88@gmail.com>
2025-05-25 21:46:29 -0300

    feat(AuthController): injeção de componentes via construtor

--- src/main/java/com/ms/ms_user/controllers/AuthController.java
+++ src/main/java/com/ms/ms_user/controllers/AuthController.java
@@ -1,10 +1,31 @@
 package com.ms.ms_user.controllers;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.ms_user.repositories.UserRepository;
+import com.ms.ms_user.security.JWTUtil;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    
+
+    final AuthenticationManager authenticationManager;
+
+    final UserRepository userRepository;
+
+    final PasswordEncoder passEncoder;
+
+    final JWTUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
+            PasswordEncoder passwordEncoder, JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
 }
