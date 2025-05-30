89dafa86c46afcfc7f1f38a16a38ae2a91bc86bc
bbering <bbering88@gmail.com>
2025-05-29 22:55:01 -0300

    fix(UserService): corrigido metodo para utilizar o email do user ao invés do username

--- src/main/java/com/ms/ms_user/service/UserService.java
+++ src/main/java/com/ms/ms_user/service/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ms.ms_user.dtos.UserRequestDTO;
@@ -24,16 +25,19 @@ import jakarta.transaction.Transactional;
 public class UserService implements UserDetailsService {
 
     final UserRepository userRepository;
-
     final UserProducer userProducer;
-
     private final AuthenticationManager authenticationManager;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    // injeção com a tag @Lazy para evitar ciclo de dependencias
-    public UserService(UserRepository userRepository, UserProducer userProducer, @Lazy AuthenticationManager authManager) {
+    // injecao com @Lazy para evitar ciclo de dependencia
+    public UserService(UserRepository userRepository,
+            UserProducer userProducer,
+            @Lazy AuthenticationManager authManager,
+            BCryptPasswordEncoder passwordEncoder) {
         this.authenticationManager = authManager;
         this.userProducer = userProducer;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User toEntity(UserRequestDTO userRequest) {
@@ -48,30 +52,29 @@ public class UserService implements UserDetailsService {
         return userDataToReturn;
     }
 
-    // metodo auxiliar para ser utilizado no processo de autenticação
     public String authenticate(String username, String rawPassword) {
-    Authentication authentication = authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(username, rawPassword)
-    );
-    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-    return userDetails.getUsername();
-}
-
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, rawPassword));
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
 
     @Transactional
     public UserResponseDTO saveNewUser(UserRequestDTO userData) {
         User userToSave = toEntity(userData);
-        UserResponseDTO userDataToReturn = toDTO(userToSave);
+
+        userToSave.setPassword(passwordEncoder.encode(userToSave.getPassword()));
+
         userRepository.save(userToSave);
 
         userProducer.publishEmailMessage(userData);
 
-        return userDataToReturn;
+        return toDTO(userToSave);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userToFind = userRepository.findUserByName(username);
+        User userToFind = userRepository.findUserByEmail(username);
 
         if (userToFind == null) {
             throw new UsernameNotFoundException("User not found");
@@ -79,7 +82,7 @@ public class UserService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(
                 userToFind.getName(),
-                userToFind.getEmail(),
+                userToFind.getPassword(),
                 Collections.emptyList());
     }
 }
