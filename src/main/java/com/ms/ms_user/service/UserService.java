9dae640cf7cc25e62570e43749163fd5f1292e89
bbering <bbering88@gmail.com>
2025-06-02 18:47:37 -0300

    refactor(UserService): separação de responsabilidades entre a service principal e a customizada

--- src/main/java/com/ms/ms_user/service/UserService.java
+++ src/main/java/com/ms/ms_user/service/UserService.java
@@ -1,42 +1,35 @@
 package com.ms.ms_user.service;
 
-import java.util.Collections;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.ms.ms_user.dtos.UserRequestDTO;
 import com.ms.ms_user.dtos.UserResponseDTO;
 import com.ms.ms_user.models.User;
 import com.ms.ms_user.producers.UserProducer;
 import com.ms.ms_user.repositories.UserRepository;
-
 import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
 
     final UserRepository userRepository;
     final UserProducer userProducer;
     private final AuthenticationManager authenticationManager;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    // injecao com @Lazy para evitar ciclo de dependencia
     public UserService(UserRepository userRepository,
-            UserProducer userProducer,
-            @Lazy AuthenticationManager authManager,
-            BCryptPasswordEncoder passwordEncoder) {
-        this.authenticationManager = authManager;
-        this.userProducer = userProducer;
+                       UserProducer userProducer,
+                       @Lazy AuthenticationManager authenticationManager,
+                       BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
+        this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -54,7 +47,8 @@ public class UserService implements UserDetailsService {
 
     public String authenticate(String username, String rawPassword) {
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(username, rawPassword));
+                new UsernamePasswordAuthenticationToken(username, rawPassword)
+        );
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return userDetails.getUsername();
     }
@@ -62,27 +56,9 @@ public class UserService implements UserDetailsService {
     @Transactional
     public UserResponseDTO saveNewUser(UserRequestDTO userData) {
         User userToSave = toEntity(userData);
-
         userToSave.setPassword(passwordEncoder.encode(userToSave.getPassword()));
-
         userRepository.save(userToSave);
-
         userProducer.publishEmailMessage(userData);
-
         return toDTO(userToSave);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userToFind = userRepository.findUserByEmail(username);
-
-        if (userToFind == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-
-        return new org.springframework.security.core.userdetails.User(
-                userToFind.getEmail(), // corrigido o campo exigido para efetuar o login, substituindo name por email
-                userToFind.getPassword(),
-                Collections.emptyList());
     }
 }
