b696e6d86c364fac1939cafbc2291ef47b563029
bbering <bbering88@gmail.com>
2025-07-01 15:55:19 -0300

    feat(UserRequestDTO): adicionada validação NotBlank

--- src/main/java/com/ms/ms_user/dtos/UserRequestDTO.java
+++ src/main/java/com/ms/ms_user/dtos/UserRequestDTO.java
@@ -5,11 +5,19 @@ import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class UserRequestDTO {
     private UUID userId;
+
+    @NotBlank(message = "Email é obrigatório")
     private String email;
+
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
+
+    @NotBlank(message = "Senha é obrigatória")
     private String password;
 }
